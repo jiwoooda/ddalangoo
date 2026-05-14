b64bade887104ae44aea898dc9eb089d11fb972b
synuo <synuolee@gmail.com>
2026-05-14 17:09:28 +0900

    Move frontend code from demo-repository

---
+++ ddalangoo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import flutter_tts
+import record_darwin
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
---
+++ ddalangoo/test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('placeholder test', (WidgetTester tester) async {
+    // TODO: 나중에 실제 테스트 추가
+    expect(true, isTrue);
+  });
+}
