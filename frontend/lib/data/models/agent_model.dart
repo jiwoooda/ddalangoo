36fabef4e7ce3577a0500b634b6a4eb4cf7158a3
synuo <synuolee@gmail.com>
2026-05-25 14:13:16 +0900

    fix. update agent_model.dart

--- frontend/lib/data/models/agent_model.dart
+++ frontend/lib/data/models/agent_model.dart
@@ -114,7 +114,6 @@ class AgentResponse {
   final dynamic pendingConfirmation;
   final dynamic availableOptions;
   final dynamic deliveryAddress;
-  final dynamic cart;
   final dynamic order;
   final dynamic payment;
   final dynamic uiCommand;
@@ -132,7 +131,6 @@ class AgentResponse {
     this.pendingConfirmation,
     this.availableOptions,
     this.deliveryAddress,
-    this.cart,
     this.order,
     this.payment,
     this.uiCommand,
@@ -153,7 +151,6 @@ class AgentResponse {
     pendingConfirmation: json['pendingConfirmation'],
     availableOptions: json['availableOptions'],
     deliveryAddress: json['deliveryAddress'],
-    cart: json['cart'],
     order: json['order'],
     payment: json['payment'],
     uiCommand: json['uiCommand'],
